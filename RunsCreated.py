56263268d1dda11b5f3a285a0e1418607faaaf69
Tyler R. Dodgen <58264866+dodgen92@users.noreply.github.com>
2022-11-06 21:27:58 -0500

    Added option for babip calculation
    batting average on balls in play

--- RunsCreated.py
+++ RunsCreated.py
@@ -1,16 +1,30 @@
-bb = float(input('Base on Balls: '))
-hbp = float(input('Hit by Pitch: '))
-singles = float(input('Singles: '))
-doubles = float(input('Doubles: '))
-tripples = float(input('Tripples: '))
-hr = float(input('Home Runs: '))
-ab = float(input('At bats: '))
-sf= float(input('Sac Flys: '))
-ibb= float(input('Intentional Base on Balls: '))
-ubb = bb - ibb
+print ("Select which statistic to calculate")
+print ("1. wOBA")
+print ("2. BABIP")
+print ("3. Pythagorean Wins")
+operation = input()
 
 
-woba = float (((ubb * .69) + (.722 * hbp) + (.888 * singles) + (1.271 * doubles) + (1.616 * tripples) + (2.101 * hr)) / (ab + bb - ibb + sf + hbp))
 
+if operation == "1":
+    bb = float(input('Base on balls: '))
+    hbp = float(input('Hit by pitch: '))
+    singles = float(input('Singles: '))
+    doubles = float(input('Doubles: '))
+    tripple = float(input('Tripples: '))
+    hr = float(input('Home Runs: '))
+    ab = float(input('At bats: '))
+    sf = float(input('Sac Flys: '))
+    ibb = float(input('Intentional Walks: '))
+    ubb = bb - ibb
+    woba = float(((ubb * .69) + (.722 * hbp) + (.888 * singles) + (1.271 * doubles) + (1.616 * tripple) + (2.101 * hr)) / (ab + bb - ibb + sf + hbp))
+    print(woba)
 
-print(woba)
+elif operation == "2":
+    hits = float(input('Hits: '))
+    hrs = float(input('Home Runs: '))
+    ks = float(input('Strikeouts '))
+    ab = float(input('At bats: '))
+    sf = float(input('Sac Flys: '))
+    babip = (hits - hrs) / (ab - hrs - ks + sf)
+    print(babip)
